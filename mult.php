a8245bc4a4c009053f9274cf2b6d5b2a50eff77c
Marks <nxmarks@labs.neit.edu>
2018-03-28 10:35:22 -0400

    Finished Table

--- mult.php
+++ mult.php
@@ -3,9 +3,21 @@ $size = mt_rand(5, 10);
 $table = "<table>\n";
 for ($rows = 1; $rows <= $size; $rows++)
 {
-	$table .= "\t<tr>";
+	$table .= "\t<tr>\n";
+	for($cols = 1; $cols <= $size; $cols++):
+	$table .= "<td>" . $rows*$cols . "</td>\n";
+	endfor;
 	$table .= "\t</tr>\n";
 }
 $table .= "</table>\n";
-echo $table;
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Multiplication Table</title>
+</head>
+<body>
+<?php echo $table; ?>
+</body>
+</html>
